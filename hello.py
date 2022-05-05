670bd4145471d1ea85ee9e5d05357188f30a82a4
letscodee <nmehta9@usfca.edu>
2022-05-05 15:30:11 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,20 +1,18 @@
 import click
 import glob
 
+
 @click.command()
 @click.option(
   "--path", prompt = "Path to search for csv files", help = "This is path search for files"
 )
-
 @click.option(
   "--ftype", prompt = "Pass in the type of file you want to search", help = "This is path search for files"
 )
-
 def search(path, ftype):
   res = glob.glob(f"{path}/*.{ftype}")
-  click.echo(click.style("Found Matches", fg="red")
+  click.echo(click.style("Found Matches", fg="red"))
   for result in res:
-      click.echo(click.style(f"{result}", fg="white", bg="yellow"
+      click.echo(click.style(f"{result}", fg="white", bg="yellow"))
 if __name__ == "__main__":
-   search()
-  
+    search()
